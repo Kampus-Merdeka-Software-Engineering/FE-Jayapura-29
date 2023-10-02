0270dddefda4aa5af32313e2962ddca153068bdd
josiahfs <josiahfrlswt@gmail.com>
2023-10-02 21:45:38 +0700

    add login and signup

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+// menu.onclick = () =>{
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// }
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+// window.onscroll = () => {
+//   menu.classList.remove("fa-times");
+//   navbar.classList.remove("active");
+// };
+
+// get the navbar
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((navbarHTML) => {
+      document.getElementById("navbar-container").innerHTML = navbarHTML;
+    });
+});
+
+// get the footer
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then((navbarHTML) => {
+      document.getElementById("footer-container").innerHTML = navbarHTML;
+    });
+});
