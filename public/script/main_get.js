9451211ebef591983b329445ce5f1211b5ebc0d1
BintangRF <bintangrama777@gmail.com>
2023-10-09 02:33:02 +0700

    tambahan

--- public/script/main_get.js
+++ public/script/main_get.js
@@ -49,6 +49,67 @@ fetch(`${backendUrl}/signup`, {
     console.error("Terjadi kesalahan:", error);
   });
 
+// Fetch GET
+fetch(`${backendUrl}/article1`, {
+  method: "GET",
+})
+  .then((response) => response.text())
+  .then((html) => {
+    // Memasukkan HTML halaman sign up ke dalam elemen DOM yang sesuai, misalnya:
+    document.getElementById("article1-container").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Terjadi kesalahan:", error);
+  });
+//
+fetch(`${backendUrl}/article2`, {
+  method: "GET",
+})
+  .then((response) => response.text())
+  .then((html) => {
+    // Memasukkan HTML halaman sign up ke dalam elemen DOM yang sesuai, misalnya:
+    document.getElementById("article2-container").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Terjadi kesalahan:", error);
+  });
+
+fetch(`${backendUrl}/article3`, {
+  method: "GET",
+})
+  .then((response) => response.text())
+  .then((html) => {
+    // Memasukkan HTML halaman sign up ke dalam elemen DOM yang sesuai, misalnya:
+    document.getElementById("article3-container").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Terjadi kesalahan:", error);
+  });
+
+fetch(`${backendUrl}/article4`, {
+  method: "GET",
+})
+  .then((response) => response.text())
+  .then((html) => {
+    // Memasukkan HTML halaman sign up ke dalam elemen DOM yang sesuai, misalnya:
+    document.getElementById("article4-container").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Terjadi kesalahan:", error);
+  });
+
+fetch(`${backendUrl}/team`, {
+  method: "GET",
+})
+  .then((response) => response.text())
+  .then((html) => {
+    // Memasukkan HTML halaman sign up ke dalam elemen DOM yang sesuai, misalnya:
+    document.getElementById("team-container").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Terjadi kesalahan:", error);
+  });
+
 //
 fetch(`${backendUrl}/login`)
   .then((response) => {
@@ -121,32 +182,32 @@ fetch(`${backendUrl}/edit_profile`, {
   });
 
 // Check Login Status
-fetch(`${backendUrl}/check-login-status`, {
-  method: "GET",
-})
-  .then((response) => response.json())
-  .then((data) => {
-    const isLoggedIn = data.isLoggedIn;
-    if (isLoggedIn) {
-      // Pengguna sudah login, lakukan aksi yang sesuai
-      // Contoh: tampilkan navbar setelah login
-      fetch("${backendUrl}/navbar-after-login")
-        .then((response) => response.text())
-        .then((html) => {
-          const navbarContainer2 = document.getElementById("navbar-container");
-          navbarContainer2.innerHTML = html;
-        });
-    } else {
-      // Pengguna belum login, lakukan aksi yang sesuai
-      // Contoh: tampilkan navbar sebelum login
-      fetch(`${backendUrl}/navbar-before-login`)
-        .then((response) => response.text())
-        .then((html) => {
-          const navbarContainer = document.getElementById("navbar-container");
-          navbarContainer.innerHTML = html;
-        });
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// fetch(`${backendUrl}/check-login-status`, {
+//   method: "GET",
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const isLoggedIn = data.isLoggedIn;
+//     if (isLoggedIn) {
+//       // Pengguna sudah login, lakukan aksi yang sesuai
+//       // Contoh: tampilkan navbar setelah login
+//       fetch("${backendUrl}/navbar-after-login")
+//         .then((response) => response.text())
+//         .then((html) => {
+//           const navbarContainer2 = document.getElementById("navbar-container");
+//           navbarContainer2.innerHTML = html;
+//         });
+//     } else {
+//       // Pengguna belum login, lakukan aksi yang sesuai
+//       // Contoh: tampilkan navbar sebelum login
+//       fetch(`${backendUrl}/navbar-before-login`)
+//         .then((response) => response.text())
+//         .then((html) => {
+//           const navbarContainer = document.getElementById("navbar-container");
+//           navbarContainer.innerHTML = html;
+//         });
+//     }
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
